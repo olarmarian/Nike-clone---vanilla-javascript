23c71752679760fb3ca8a7fbf5a204df9eb33237
olarmarian <olar.marian7@gmail.com>
2019-10-09 01:17:38 +0300

    update homepage product list

--- products.js
+++ products.js
@@ -4,7 +4,8 @@ const adsSection = document.querySelector('.advertisment-section')
 const overlayMenu = document.querySelector(".fa-bars");
 const overlayMenuDOM = document.querySelector(".menu-overlay");
 const overlayMenuList = document.querySelector(".menu-overlay-list");
-
+const homepageProductsList = document.querySelectorAll(".homepage-products-list");
+const homepageProductsListTitles = document.querySelectorAll(".homepage-products-list-title");
 class UI{
     constructor(){
         this.condition = true;
@@ -30,14 +31,11 @@ class UI{
     }
 }
 
-let open = false; // condition for showing the overlay menu
-let once = 0;
 let showMenu = false;
 
 document.addEventListener("DOMContentLoaded",()=>{
     const ui = new UI();
     ui.adsLoop();
-    
     
     function handleMenu(event){
         localStorage.setItem("genre",event.target.innerText.toLowerCase());
@@ -60,6 +58,17 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
         showMenu = !showMenu;
     })
-    
-    
+    homepageProductsList.forEach(list =>{
+        list.children[0].addEventListener('click', () =>{
+            for(let i = 1 ; i < list.children.length; i ++){
+                if(list.children[i].classList.contains("homepage-products-list-item") && !list.children[i].classList.contains("display-list-items")){
+                    list.children[i].classList.add("display-list-items")
+                    list.children[i].classList.remove("hide-list-items")
+                }else if(list.children[i].classList.contains("homepage-products-list-item") && list.children[i].classList.contains("display-list-items")){
+                    list.children[i].classList.remove("display-list-items")
+                    list.children[i].classList.add("hide-list-items")
+                }          
+            }
+        });
+    })
 })
